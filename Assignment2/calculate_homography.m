05293dc8cc361fb88fbe9d624b490567cd02330b
Ludwig Lorenz <ludwig.david.lorenz@uni-weimar.de>
2020-11-27 15:51:20 +0100

    fixed shearing calculation

--- Assignment2/calculate_homography.m
+++ Assignment2/calculate_homography.m
@@ -4,26 +4,32 @@ function h = calculate_homography(p1, p2)
   p2 = [p2, [1, 1, 1, 1]'];
   
   # conditioning
-  t1 = [mean(p1(:,1)), mean(p1(:,2))];
-  t2 = [mean(p2(:,1)), mean(p2(:,2))];
-  s1 = [mean_dist(p1(:,1), t1(1)), mean_dist(p1(:,2), t1(2))];
-  s2 = [mean_dist(p2(:,1), t2(1)), mean_dist(p2(:,2), t2(2))];
-  T1 = [1/s1(1) ,0, - t1(1)/s1(1); 0, 1/s1(2), - t1(2)/s1(2); 0, 0, 1];
-  T2 = [1/s2(1) ,0, - t2(1)/s2(1); 0, 1/s2(2), - t2(2)/s2(2); 0, 0, 1];
+  t1 = [mean(p1(:,1)), mean(p1(:,2))]
+  t2 = [mean(p2(:,1)), mean(p2(:,2))]
+  s1 = [mean_dist(p1(:,1), t1(1)), mean_dist(p1(:,2), t1(2))]
+  s2 = [mean_dist(p2(:,1), t2(1)), mean_dist(p2(:,2), t2(2))]
+  T1 = [1/s1(1) ,0, - t1(1)/s1(1); 0, 1/s1(2), - t1(2)/s1(2); 0, 0, 1]
+  T2 = [1/s2(1) ,0, - t2(1)/s2(1); 0, 1/s2(2), - t2(2)/s2(2); 0, 0, 1]
   
+  # algorithm
   A = [];
   for i = [1, 2, 3, 4]
-    pa = T1*p1(i,:)';
+    pa = T1*p1(i,:)'
     pb = T2*p2(i,:)';
     A_i = [-pb(3)*pa', [0, 0, 0], pb(1)*pa';
-           [0, 0, 0], -pb(3)*pa', pb(2)*pa'];
+           [0, 0, 0], -pb(3)*pa', pb(2)*pa']
     A = [A;A_i];
   endfor
+  # um A quadratisch zu machen, müssen wir eine Zeile ergänzen
+  A =  [A; [0,0,0,0,0,0,0,0,0]]
   [U,S,V] = svd(A)
-  hs = V'(:,9)
+  # hs is the Eigenvector, V is transposed
+  hs = V(:,9)
   h = [hs(1), hs(2), hs(3); hs(4), hs(5), hs(6); hs(7), hs(8), hs(9)]
   # deconditioning
   h = inv(T2) * h * T1
+  # TODO euclidization
+  h = 
 endfunction
 
 
@@ -32,5 +38,5 @@ function s = mean_dist(p, t)
   for i = [1, 2, 3, 4]
     s = s + abs(p(i) - t);
   endfor
-  s = 4/s 
+  s = s/4;
 endfunction
--- Assignment2/photovision.m
+++ Assignment2/photovision.m
@@ -2,10 +2,10 @@ im1 = imread('bilder/image_1.jpg', 'jpg');
 im2 = imread('bilder/image_2.jpg', 'jpg');
 im3 = imread('bilder/image_3.jpg', 'jpg');
 
-%p1 = [104.552, 65.881; 2204.165, 1719.558; 3746.358, 75.171; 3895.003, 112.332];
-%p2 = [151.00, 1756.72; 2064.81, 121.62; 3802.10, 1710.27; 3792.81, 1728.85];
+p1 = [93,617; 729,742; 703,1233; 152,1103];
+p2 = [0,0; 639, 0; 639, 639; 0, 639];
 
-[p1, p2] = find_points(im1, im2)
+% [p1, p2] = find_points(im1, im2)
 h = calculate_homography(p1, p2)
 imshow(geokor(h, im1, im2))
 
